726ee99d5745666a851f67f0d3dc22f9a284e28a
KAPIL-JHALANI <kapiljhalani009@gmail.com>
2020-02-01 02:29:13 +0530

    relaunched

---
+++ App2/app/src/main/java/com/example/app2/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.app2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    private static final int VIDEO_CAPTURE = 101;
+
+    StorageReference mstorage;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        mstorage=FirebaseStorage.getInstance().getReference();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(intent, VIDEO_CAPTURE);
+
+
+            }
+
+
+        });
+
+
+    }
+
+
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri videoUri = data.getData();
+
+        Log.i("heelo", "hit");
+        StorageReference storageRef = mstorage.child("videos");
+
+        storageRef.putFile(videoUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(MainActivity.this,"upload successful",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,"not",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        Log.i("hey", videoUri.toString());
+        if (requestCode == VIDEO_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video saved to:\n" +
+                        videoUri, Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+}
