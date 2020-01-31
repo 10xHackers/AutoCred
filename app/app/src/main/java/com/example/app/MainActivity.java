ea8d5983b21fb743e6183f5d8d53589f76bfcf28
imdhruvgupta <gdhruv167@gmail.com>
2020-01-31 15:15:44 +0530

    navigate button

--- app/app/src/main/java/com/example/app/MainActivity.java
+++ app/app/src/main/java/com/example/app/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
- public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    Button captureButton;
+    ImageView imageView;
+    Button navigateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captureButton = findViewById(R.id.button1);
+        navigateButton = findViewById(R.id.button2);
+        imageView = findViewById(R.id.imageView1);
+
+        captureButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
+//        navigateButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent();
+//
+//            }
+//        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 }
