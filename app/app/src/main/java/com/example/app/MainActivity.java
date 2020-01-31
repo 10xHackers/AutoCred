7ef8b53e35412807f0924d0186cbaa24ea72d9d0
KAPIL-JHALANI <kapiljhalani009@gmail.com>
2020-01-31 19:00:38 +0530

    Image finally got uploaded!

--- app/app/src/main/java/com/example/app/MainActivity.java
+++ app/app/src/main/java/com/example/app/MainActivity.java
@@ -1,32 +1,52 @@
 package com.example.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int CAMERA_REQUEST = 1888;
+    private static final int CAMERA_REQUEST_CODE = 1;
     Button captureButton;
     ImageView imageView;
     Button navigateButton;
 
-     Button pay;
+    private StorageReference mstorage;
+    private ProgressDialog mprogress;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mstorage=FirebaseStorage.getInstance().getReference();
         captureButton = findViewById(R.id.button1);
         navigateButton = findViewById(R.id.button2);
         imageView = findViewById(R.id.imageView1);
+        mprogress=new ProgressDialog(this);
 
         captureButton.setOnClickListener(new View.OnClickListener()
         {
@@ -34,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v)
             {
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
             }
         });
 
@@ -50,10 +70,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
-        {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK)
+       {
+//            Bitmap photo = (Bitmap) data.getExtras().get("data");
+//            imageView.setImageBitmap(photo);
+           mprogress.setMessage("progress upload....");
+           mprogress.show();
+
+           // Uri uri=data.getData();
+           Bundle extras = data.getExtras();
+           Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+           ByteArrayOutputStream baos = new ByteArrayOutputStream();
+           bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+           byte[] dataBAOS = baos.toByteArray();
+
+           imageView.setImageBitmap(bitmap);
+
+
+           StorageReference imagesRef = mstorage.child("filename" + new Date().getTime());
+
+            imagesRef.putBytes(dataBAOS).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(MainActivity.this,"upload successful",Toast.LENGTH_SHORT).show();
+                    mprogress.dismiss();
+                }
+            });
+
+
+
         }
     }
 }
